06c573533ed234c62955b1507541bfef695bf33f
sekine104 <sekine104@outlook.jp>
2024-03-02 00:55:27 +0900

    warning表示を統一された関数に置き換えた

--- imgdler/imdgler.py
+++ imgdler/imdgler.py
@@ -1,15 +1,15 @@
 from imgdler.imgdler import Downloadable, ImageDownloader
 
-def __warning():
-    from rich.console import Console
-    from rich.text import Text
-    __duplicated_warn = """Warning: `imdgler.py` will be deprecated in v3.0.0~.
+
+def __error():
+    from .errors import warning
+
+    warning(""""Warning: `imdgler.py` will be deprecated in v3.0.0~.
          If you are using `imdgler.py`, please change it to `imgdler.py`.
          There are no changes to the following classes: {}, {}
-""".format(
+    """.format(
         Downloadable.__name__,
         ImageDownloader.__name__,
-    )
-    Console().print(Text(__duplicated_warn, "#f0f033"))
+    ))
 
-__warning()
+__error()
